2fa27b4f2406027f19a8f49c3b367c36898e4059
Levi Cameron <levi.cameron@alliancesoftware.com.au>
2016-07-15 13:17:09 +1000

    Code style cleanup

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 setup(
-  name = 'allianceutils',
-  packages = ['allianceutils'], # this must be the same as the name above
-  version = '0.1.1',
-  description = 'Utils for django',
-  author = 'Alliance Software',
-  author_email = 'support@alliancesoftware.com.au',
-  url = 'http://gitlab.internal.alliancesoftware.com.au/alliance/alliance-django-utils',
-  download_url = 'http://gitlab.internal.alliancesoftware.com.au/alliance/alliance-django-utils/repository/archive.tar.gz?ref=0.1.1',
-  keywords = ['django'], # arbitrary keywords
-  classifiers = [],
+    name='allianceutils',
+    packages=['allianceutils'], # this must be the same as the name above
+    version='0.1.1',
+    description='Utils for django',
+    author='Alliance Software',
+    author_email='support@alliancesoftware.com.au',
+    url='http://gitlab.internal.alliancesoftware.com.au/alliance/alliance-django-utils',
+    download_url='http://gitlab.internal.alliancesoftware.com.au/alliance/alliance-django-utils/repository/archive.tar.gz?ref=0.1.1',
+    keywords=['alliance', 'django'], # arbitrary keywords
+    classifiers=[],
 )
