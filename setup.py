18c5db3cdf895f91dedc38d8d211d3f88168b30a
Levi Cameron <levi.cameron@alliancesoftware.com.au>
2016-08-02 02:54:30 +1000

    Version bump

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ except ImportError:
 
 setup(
     name='allianceutils',
-    version='0.0.1',
+    version='0.1.2',
     author='Alliance Software',
     author_email='support@alliancesoftware.com.au',
     packages=['allianceutils'], # this must be the same as the name above
@@ -22,5 +22,9 @@ setup(
     url='http://gitlab.internal.alliancesoftware.com.au/alliance/alliance-django-utils',
     classifiers=[],
     download_url='http://gitlab.internal.alliancesoftware.com.au/alliance/alliance-django-utils/repository/archive.tar.gz?ref=0.1.1',
-    keywords='alliance alliancesoftware django',
+    keywords=[
+		'alliance',
+		'alliancesoftware',
+		'django',
+	],
 )
