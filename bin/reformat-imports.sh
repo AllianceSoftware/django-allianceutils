a072a6821e0a4fc76c0d3e7e3f0c3751be584e22
Levi Cameron <levi.cameron@alliancesoftware.com.au>
2017-02-27 01:51:35 +1100

    Testing new CI system

--- bin/reformat-imports.sh
+++ bin/reformat-imports.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 set -o pipefail
 
-source "$(dirname "$BASH_SOURCE")/common.inc"
+source "$(dirname "${BASH_SOURCE[0]}")/common.inc"
 
 if [[ "$1" = "--check-only" ]] ; then
 	CHECK_ONLY="--check-only"
@@ -15,7 +15,7 @@ else
 fi
 
 # first try symlink in this dir
-ISORT="$(dirname "$BASH_SOURCE")/isort"
+ISORT="$(dirname "${BASH_SOURCE[0]}")/isort"
 
 # then the current virtualenv
 if ! [ -x "$ISORT" ] ; then
@@ -29,7 +29,7 @@ fi
 
 if ! [ -x $ISORT ] ; then
 	echo "Can't find isort; this may not work trying to commit from a GUI" >&2
-	echo "You can try symlinking isort in the $(dirname "$BASH_SOURCE") directory" >&2
+	echo "You can try symlinking isort in the $(dirname "${BASH_SOURCE[0]}") directory" >&2
 	exit 1
 fi
 
