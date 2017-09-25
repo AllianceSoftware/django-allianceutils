6c9f5ed414234040cd1fe1643f0c91fba59113a0
Levi Cameron <levi.cameron@alliancesoftware.com.au>
2017-09-25 14:24:55 +1000

    sys: use standard django test runner
    allows pycharm test runner compatibility

---
+++ allianceutils/tests/settings.py
@@ -0,0 +1,94 @@
+import hashlib as _hashlib
+import os as _os
+import random as _random
+import sys as _sys
+
+# import django
+
+is_ci = _os.environ.get('CI_SERVER', 'no') == 'yes'
+
+
+BASE_DIR = _os.path.dirname(__file__)
+
+_db_vars = {
+    'HOST': ('MYSQL_HOST', None),
+    'USER': ('MYSQL_USER', None),
+    'PASSWORD': ('MYSQL_PASSWORD', None),
+    'NAME': ('MYSQL_DATABASE', 'alliancedjangoutils'),
+}
+_db_vars = {var: _os.environ.get(env_var, default) for var, (env_var, default) in _db_vars.items()}
+_db_vars = {key: value for key, value in _db_vars.items() if value}
+_db_vars['ENGINE'] = 'django.db.backends.mysql'
+_db_vars['OPTIONS'] = {
+    'init_command': 'SET default_storage_engine=InnoDB',
+    'read_default_file': '~/.my.cnf',
+}
+
+# Django connects via the live DB in order to create/drop the test DB
+# If the live DB doesn't exist then it bails out before even trying to
+# create the test DB, so this doesn't really work
+# if is_ci:
+#     db_vars['TEST'] = {
+#         'NAME': db_vars['NAME'],
+#     }
+
+DATABASES = {'default': _db_vars}
+
+print(DATABASES)
+
+INSTALLED_APPS = [
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'allianceutils',
+    'allianceutils.tests.autodumpdata',
+    'allianceutils.tests.serializers',
+    'allianceutils.tests.util',
+]
+
+MIDDLEWARE_CLASSES = []
+
+TEMPLATE_DIRS = [
+    # os.path.join(BASE_DIR, 'compat/tests/templates/')
+]
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'APP_DIRS': True,
+        'DIRS': TEMPLATE_DIRS,
+    },
+]
+
+SERIALIZATION_MODULES = {
+    'json_ordered': 'allianceutils.serializers.json_ordered',
+    'json': 'allianceutils.serializers.json_orminheritancefix',
+}
+
+#ROOT_URLCONF = 'tests.urls',
+
+NOSE_ARGS=[
+    # dont suppress stdout
+    '--nocapture',
+    '--nologcapture',
+]
+
+SECRET_KEY = _hashlib.sha256(str(_random.SystemRandom().getrandbits(256)).encode('ascii')).hexdigest()
+
+
+# def runtests(*test_args):
+#
+#     django.setup()
+#
+#     import django_nose  # has to be imported after django config happens
+#
+#     runner = django_nose.NoseTestSuiteRunner(
+#         verbosity=2,
+#         interactive=not is_ci,
+#     )
+#     failures = runner.run_tests(test_args)
+#     sys.exit(failures)
+#
+#
+# if __name__ == '__main__':
+#     setup()
+#     runtests(*sys.argv[1:])
--- runtests.py
+++ runtests.py
@@ -1,109 +1,12 @@
-"""
-This script is a trick to setup a fake Django environment, since this reusable
-app will be developed and tested outside any specific Django project.
-Via ``settings.configure`` you will be able to set all necessary settings
-for your app and run the tests as if you were calling ``./manage.py test``.
-
-Adapted from django-compat
-"""
-from __future__ import print_function
-
+#!/usr/bin/env python
 import os
 import sys
 
-import django
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "allianceutils.tests.settings")
 
-is_ci = os.environ.get('CI_SERVER', 'no') == 'yes'
-
-
-def setup():
-    BASE_DIR = os.path.dirname(__file__)
-
-    db_vars = {
-        'HOST': ('MYSQL_HOST', None),
-        'USER': ('MYSQL_USER', None),
-        'PASSWORD': ('MYSQL_PASSWORD', None),
-        'NAME': ('MYSQL_DATABASE', 'alliancedjangoutils'),
-    }
-    db_vars = {var: os.environ.get(env_var, default) for var, (env_var, default) in db_vars.items()}
-    db_vars = {key: value for key, value in db_vars.items() if value}
-    db_vars['ENGINE'] = 'django.db.backends.mysql'
-    db_vars['OPTIONS'] = {
-        'init_command': 'SET default_storage_engine=InnoDB',
-        'read_default_file': '~/.my.cnf',
-    }
-
-    # Django connects via the live DB in order to create/drop the test DB
-    # If the live DB doesn't exist then it bails out before even trying to
-    # create the test DB, so this doesn't really work
-    # if is_ci:
-    #     db_vars['TEST'] = {
-    #         'NAME': db_vars['NAME'],
-    #     }
-
-    DATABASES = {'default': db_vars}
-
-    INSTALLED_APPS = [
-        'django.contrib.auth',
-        'django.contrib.contenttypes',
-        'allianceutils',
-        'allianceutils.tests.autodumpdata',
-        'allianceutils.tests.serializers',
-        'allianceutils.tests.util',
-    ]
-
-    MIDDLEWARE_CLASSES = []
-
-    TEMPLATE_DIRS = [
-        # os.path.join(BASE_DIR, 'compat/tests/templates/')
-    ]
-
-    TEMPLATES = [
-        {
-            'BACKEND': 'django.template.backends.django.DjangoTemplates',
-            'APP_DIRS': True,
-            'DIRS': TEMPLATE_DIRS,
-        },
-    ]
-
-    SERIALIZATION_MODULES = {
-        'json_ordered': 'allianceutils.serializers.json_ordered',
-        'json': 'allianceutils.serializers.json_orminheritancefix',
-    }
-
-    from django.conf import settings
-
-    if not settings.configured:
-        settings.configure(
-            INSTALLED_APPS=INSTALLED_APPS,
-            DATABASES=DATABASES,
-            ROOT_URLCONF='allianceutils.tests.urls',
-            MIDDLEWARE_CLASSES=MIDDLEWARE_CLASSES,
-            TEMPLATE_DIRS=TEMPLATE_DIRS,
-            TEMPLATES=TEMPLATES,
-            SERIALIZATION_MODULES=SERIALIZATION_MODULES,
-            NOSE_ARGS=[
-                # dont suppress stdout
-                '--nocapture',
-                '--nologcapture',
-            ],
-        )
-
-
-def runtests(*test_args):
-
-    django.setup()
-
-    import django_nose  # has to be imported after django config happens
-
-    runner = django_nose.NoseTestSuiteRunner(
-        verbosity=2,
-        interactive=not is_ci,
-    )
-    failures = runner.run_tests(test_args)
-    sys.exit(failures)
-
+#sys.path.append()
 
 if __name__ == '__main__':
-    setup()
-    runtests(*sys.argv[1:])
+    from django.core.management import execute_from_command_line
+    print(sys.argv)
+    execute_from_command_line([sys.argv[0], 'test'] + sys.argv[1:])
