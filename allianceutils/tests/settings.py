88ae868953a9fca34d28ac11db716bda1173eef1
Levi Cameron <levi.cameron@alliancesoftware.com.au>
2017-10-05 16:15:20 +1100

    refactor: use tuples rather than lists in settings to avoid accidentally making changes

--- allianceutils/tests/settings.py
+++ allianceutils/tests/settings.py
@@ -48,19 +48,19 @@ INSTALLED_APPS = (
 
 AUTH_USER_MODEL = 'profile_auth.GenericUserProfile'
 
-MIDDLEWARE = []
+MIDDLEWARE = ()
 
-TEMPLATE_DIRS = [
+TEMPLATE_DIRS = (
     # os.path.join(BASE_DIR, 'compat/tests/templates/')
-]
+)
 
-TEMPLATES = [
+TEMPLATES = (
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'APP_DIRS': True,
         'DIRS': TEMPLATE_DIRS,
     },
-]
+)
 
 SERIALIZATION_MODULES = {
     'json_ordered': 'allianceutils.serializers.json_ordered',
@@ -68,11 +68,5 @@ SERIALIZATION_MODULES = {
 }
 
 ROOT_URLCONF = 'allianceutils.tests.urls'
-
-NOSE_ARGS=[
-    # dont suppress stdout
-    '--nocapture',
-    '--nologcapture',
-]
 
 SECRET_KEY = _hashlib.sha256(str(_random.SystemRandom().getrandbits(256)).encode('ascii')).hexdigest()
