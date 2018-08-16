f2cd1b8497bfdc2e599902688d49711ccd1839e0
Callum Anderson <callum@alliancesoftware.com.au>
2018-08-16 16:31:20 +1000

    fix: ignore ordering

--- test_allianceutils/tests/test_checks_db_constraints.py
+++ test_allianceutils/tests/test_checks_db_constraints.py
@@ -12,19 +12,20 @@ class TestCheckDBConstraints(SimpleTestCase):
         self.assertEqual(len(warnings), 2)
         app_name = 'checks_db_constraints'
         models = '{0}.CheckDBConstraintA, {0}.CheckDBConstraintB'.format(app_name)
-        expect_warning = Warning(
-            'These models have a duplicate DB constraint when truncated: '
-            'bar_equal_baz__aaaaaaaaaa__bbbbbbbbbb__cccccccccc__dddddddddd__',
-            hint='Ensure Meta.db_constraints for models %s are unique when truncated to 63 characters' % models,
-            obj=models,
-            id=ID_WARNING_DB_CONSTRAINTS,
-        )
-        self.assertEqual(warnings[0], expect_warning)
-
-        expect_warning = Warning(
-            'These models have a duplicate DB constraint when truncated: 😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀___',
-            hint='Ensure Meta.db_constraints for models %s are unique when truncated to 63 characters' % models,
-            obj=models,
-            id=ID_WARNING_DB_CONSTRAINTS,
-        )
-        self.assertEqual(warnings[1], expect_warning)
+        expect_warnings = [
+            Warning(
+                'These models have a duplicate DB constraint when truncated: '
+                'bar_equal_baz__aaaaaaaaaa__bbbbbbbbbb__cccccccccc__dddddddddd__',
+                hint='Ensure Meta.db_constraints for models %s are unique when truncated to 63 characters' % models,
+                obj=models,
+                id=ID_WARNING_DB_CONSTRAINTS,
+            ),
+            Warning(
+                'These models have a duplicate DB constraint when truncated: 😀😀😀😀😀😀😀😀😀😀😀😀😀😀😀___',
+                hint='Ensure Meta.db_constraints for models %s are unique when truncated to 63 characters' % models,
+                obj=models,
+                id=ID_WARNING_DB_CONSTRAINTS,
+            )
+        ]
+        for warning in warnings:
+            self.assertIn(warning, expect_warnings)
