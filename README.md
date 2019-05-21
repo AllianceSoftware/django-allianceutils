b49bd47646250397f90d2424640da1fd09850dc8
Fang Long <fang.long@alliancesoftware.com.au>
2019-05-21 14:03:07 +1000

    feature: django21->django22

--- README.md
+++ README.md
@@ -676,6 +676,7 @@ FIXME
         * Fix DRF BrowserableAPI causes GenericViewsetPermissions to throw out an error for None action
         * Fix `GenericUserProfileQueryset` values() and values_list() incorrectly reject all args
         * CurrentUserMiddleware now supports the post-django-1.11 MIDDLEWARE
+        * replace django 2.1 with django 2.2 (LTS)
     * 0.5.0
         * Breaking Changes    
             * drop support for python 3.4, 3.5
--- allianceutils/serializers/json_orminheritancefix/__init__.py
+++ allianceutils/serializers/json_orminheritancefix/__init__.py
@@ -12,12 +12,10 @@ elif (1, 9) <= django.VERSION < (1, 12): # Unit test passed for both 1.10 and 1.
     from .json_orminheritancefix19 import Deserializer
 elif (1, 12) <= django.VERSION < (2, 1):
     raise Exception('json_orminheritancefix has not been tested with django 2.0')
-elif (2, 1) <= django.VERSION < (2, 2):  # Unit test passed for both 1.10 and 1.11
+else:
     warnings.warn('json_orminheritancefix is not needed in django >=2.1 and will be removed', DeprecationWarning)
     from django.core.serializers.json import Serializer
     from django.core.serializers.json import Deserializer
-else:
-    raise Exception('json_orminheritancefix has not been tested with django > 2.1')
 
 __all_ = [
     'Deserializer',
