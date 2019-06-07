b6fd22542db2bbec8357339ad5c6d50036a453be
Fang Long <fang.long@alliancesoftware.com.au>
2019-06-07 16:43:03 +1000

    feature: custom staff member required decorator

---
+++ allianceutils/decorators.py
@@ -0,0 +1,6 @@
+from django.contrib.auth import REDIRECT_FIELD_NAME
+from django.contrib.admin.views.decorators import staff_member_required as original_staff_member_required
+
+# dont redirect to admin:login which breaks if admin's not present - redirect to generic login instead (which None will do).
+def staff_member_required(view_func=None, redirect_field_name=REDIRECT_FIELD_NAME, login_url=None):
+    return original_staff_member_required(view_func, redirect_field_name, login_url)
