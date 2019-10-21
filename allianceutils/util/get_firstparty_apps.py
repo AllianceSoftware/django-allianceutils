fbfc51cc6208d7957a53259efdce9bce6c66814d
Fang Long <fang.long@alliancesoftware.com.au>
2019-10-21 13:11:15 +1100

    fix: make getfirstparty apps always exclude allianceutils to bypass heroku detection issue

--- allianceutils/util/get_firstparty_apps.py
+++ allianceutils/util/get_firstparty_apps.py
@@ -5,9 +5,9 @@ from isort import SortImports
 
 def is_firstparty_app(app_config: AppConfig):
     """
-    Use isort's way of determining whether an app is "first party" or otherwise
+    Use isort's way of determining whether an app is "first party" or otherwise. exclude allianceutils.apps always (heroku detection's flawed otherwise)
     """
-    return SortImports(file_contents='').place_module(app_config.__module__) == 'FIRSTPARTY'
+    return SortImports(file_contents='').place_module(app_config.__module__) == 'FIRSTPARTY' or app_config.__module__ == 'allianceutils.apps'
 
 
 def get_firstparty_apps():
