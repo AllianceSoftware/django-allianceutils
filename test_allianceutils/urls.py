d964bc8b5222a5181bda7b434910761a1dccbf5f
Levi Cameron <levi.cameron@alliancesoftware.com.au>
2020-09-07 04:53:33 +1000

    refactor: test urls
    - use path() rather than url() for test urls
    - alphabetise urls

--- test_allianceutils/urls.py
+++ test_allianceutils/urls.py
@@ -2,15 +2,16 @@ from django.conf.urls import include
 from django.conf.urls import url
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.views import LogoutView
+from django.urls import path
 
 import test_allianceutils.tests.middleware.urls
 import test_allianceutils.tests.object_cache.urls
 import test_allianceutils.tests.profile_auth.urls
 
 urlpatterns = [
-    url(r'^accounts/login/$', LoginView.as_view(), name='login'),
-    url(r'^accounts/logout/$', LogoutView.as_view(), name='logout'),
-    url(r'^profile_auth/', include(test_allianceutils.tests.profile_auth.urls)),
-    url(r'^middleware/', include(test_allianceutils.tests.middleware.urls)),
-    url(r'^object_cache/', include(test_allianceutils.tests.object_cache.urls)),
+    path(r'accounts/login/', LoginView.as_view(), name='login'),
+    path(r'accounts/logout/', LogoutView.as_view(), name='logout'),
+    path(r'middleware/', include(test_allianceutils.tests.middleware.urls)),
+    path(r'object_cache/', include(test_allianceutils.tests.object_cache.urls)),
+    path(r'profile_auth/', include(test_allianceutils.tests.profile_auth.urls)),
 ]
